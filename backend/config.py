0dd13467ac7424cd8145842f3ef1bc162d84e6c8
Batyr Atayev <atayev2012@gmail.com>
2024-10-12 11:21:10 +0300

    Added email server settings and email template path

--- backend/config.py
+++ backend/config.py
@@ -5,4 +5,11 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+    SMTP_SERVER = os.getenv('SMTP_SERVER')
+    PORT = os.getenv('PORT')
+    SENDER_EMAIL = os.getenv('SENDER_EMAIL')
+    SENDER_EMAIL_PASSWORD = os.getenv('SENDER_EMAIL_PASSWORD')
+
+    EMAIL_TEMPLATE = 'templates/email_verification.html'
+
 config = Config()
